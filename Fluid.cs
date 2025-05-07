73c4a78377dd2e159bdbc84a11fb52d8c99c1c31
Piotr <01197645@pw.edu.pl>
2025-05-07 20:26:13 +0200

    Add support for internal walls

--- Fluid.cs
+++ Fluid.cs
@@ -11,7 +11,56 @@ const int height_with_border = height + 2;
 const float density_diffusion_rate = 5.5f;
 const float velocity_viscosity_rate = 2.5f;
 
-static void SetBoundary(BoundaryMode mode, int w, int h, Grid<float> in_out) {
+static void SetBoundary(Grid<bool> wall, BoundaryMode mode, int w, int h, Grid<float> in_out) {
+    for (int r = 1; r <= h; ++r) {
+        for (int c = 1; c <= w; ++c) {
+            if (wall[c, r]) in_out[c, r] = 0.0f;
+        }
+    }
+
+    for (int r = 1; r <= h; ++r) {
+        for (int c = 1; c <= w; ++c) {
+            if (wall[c, r]) {
+                if (!wall[c + 1, r]) {
+                    in_out[c, r] = mode == BoundaryMode.VelocityX ? -in_out[c + 1, r] : in_out[c + 1, r];
+                }
+                if (!wall[c - 1, r]) {
+                    in_out[c, r] = mode == BoundaryMode.VelocityX ? -in_out[c - 1, r] : in_out[c - 1, r];
+                }
+                if (!wall[c, r + 1]) {
+                    in_out[c, r] = mode == BoundaryMode.VelocityY ? -in_out[c, r + 1] : in_out[c, r + 1];
+                }
+                if (!wall[c, r - 1]) {
+                    in_out[c, r] = mode == BoundaryMode.VelocityY ? -in_out[c, r - 1] : in_out[c, r - 1];
+                }
+            }
+        }
+    }
+
+    // Average corners
+    for (int r = 1; r <= h; ++r) {
+        for (int c = 1; c <= w; ++c) {
+            if (!wall[c, r]) continue;
+
+            if (!wall[c + 1, r] && !wall[c, r + 1]) {
+                in_out[c, r] = 0.5f * (in_out[c + 1, r] + in_out[c, r + 1]);
+            }
+
+            if (!wall[c - 1, r] && !wall[c, r + 1]) {
+                in_out[c, r] = 0.5f * (in_out[c - 1, r] + in_out[c, r + 1]);
+            }
+
+            if (!wall[c - 1, r] && !wall[c, r - 1]) {
+                in_out[c, r] = 0.5f * (in_out[c - 1, r] + in_out[c, r - 1]);
+            }
+            
+            if (!wall[c + 1, r] && !wall[c, r - 1]) {
+                in_out[c, r] = 0.5f * (in_out[c + 1, r] + in_out[c, r - 1]);
+            }
+        }
+    }
+    
+
     for (int r = 1; r <= h; ++r) {
         in_out[    0, r] = mode == BoundaryMode.VelocityX ? -in_out[1, r] : in_out[1, r];
         in_out[w + 1, r] = mode == BoundaryMode.VelocityX ? -in_out[w, r] : in_out[w, r];
@@ -22,7 +71,7 @@ static void SetBoundary(BoundaryMode mode, int w, int h, Grid<float> in_out) {
         in_out[c, h + 1] = mode == BoundaryMode.VelocityY ? -in_out[c, h] : in_out[c, h];
     }
 
-    // Corners are averaged
+    // Average corners
     in_out[    0,     0] = 0.5f * (in_out[1,     0] + in_out[    0, 1]);
     in_out[w + 1,     0] = 0.5f * (in_out[w,     0] + in_out[w + 1, 1]);
     in_out[    0, h + 1] = 0.5f * (in_out[1, h + 1] + in_out[    0, h]);
@@ -30,12 +79,13 @@ static void SetBoundary(BoundaryMode mode, int w, int h, Grid<float> in_out) {
 }
 
 const int iteration_count = 20;
-static void Diffuse(BoundaryMode mode, int w, int h, float dt, float diffusion_rate, Grid<float> input, Grid<float> output) {
+static void Diffuse(Grid<bool> wall, BoundaryMode mode, int w, int h, float dt, float diffusion_rate, Grid<float> input, Grid<float> output) {
     float a = dt * diffusion_rate; // a = dt * diff * w * h
 
     for (int iter = 0; iter < iteration_count; ++iter) {
         for (int r = 1; r <= h; ++r) {
             for (int c = 1; c <= w; ++c) {
+                if (wall[c, r]) continue;
                 output[c, r] = (
                     input[c, r] + a * (
                         output[c - 1, r] + output[c + 1, r] + 
@@ -44,14 +94,38 @@ static void Diffuse(BoundaryMode mode, int w, int h, float dt, float diffusion_r
                 ) / (1 + 4 * a); 
             }
         }
-        SetBoundary(mode, w, h, output);
+        SetBoundary(wall, mode, w, h, output);
     }
 }
-static void Advect(BoundaryMode mode, int w, int h, float dt, Grid<float> velocityX, Grid<float> velocityY, Grid<float> input, Grid<float> output) {
+
+static Vector2 FindWall(Grid<bool> wall, int w, int h, Vector2 start, Vector2 end) {
+    Vector2 dir = end - start;
+    float len = dir.Length();
+    int steps = (int)(len) * 2 + 1;
+    Vector2 step = dir / steps;
+
+    Vector2 pos = start;
+    for (int i = 0; i < steps; ++i) {
+        Vector2 prev = pos;
+        pos += step;
+        if (pos.X <= 0.5f || pos.X >= w + 0.5f || pos.Y <= 0.5f || pos.Y >= h + 0.5f) break;
+
+        if (wall[(int)pos.X, (int)pos.Y]) {
+            pos = prev;
+            break;
+        }
+    }
+    return Vector2.Clamp(pos, new(0.5f, 0.5f), new(w + 0.5f, h + 0.5f));
+}
+
+
+static void Advect(Grid<bool> wall, BoundaryMode mode, int w, int h, float dt, Grid<float> velocityX, Grid<float> velocityY, Grid<float> input, Grid<float> output) {
     for (int r = 1; r <= h; ++r) {
         for (int c = 1; c <= w; ++c) {
-            float x = float.Clamp(c - dt * velocityX[c, r], 0.5f, w + 0.5f);
-            float y = float.Clamp(r - dt * velocityY[c, r], 0.5f, h + 0.5f);
+            if (wall[c, r]) continue;
+            var pos = FindWall(wall, w, h, new(c, r), new(c - dt * velocityX[c, r], r - dt * velocityY[c, r]));
+            float x = pos.X;
+            float y = pos.Y;
 
             int c0 = (int)x; int c1 = c0 + 1;
             int r0 = (int)y; int r1 = r0 + 1;
@@ -64,25 +138,29 @@ static void Advect(BoundaryMode mode, int w, int h, float dt, Grid<float> veloci
                 s1 * (t0 * input[c1, r0] + t1 * input[c1, r1]);
         }
     }
-    SetBoundary(mode, w, h, output);
+    SetBoundary(wall, mode, w, h, output);
 }
-static void Project(int w, int h, Grid<float> velocityX, Grid<float> velocityY, Grid<float> tempDivergance, Grid<float> tempPressure) {
+static void Project(Grid<bool> wall, int w, int h, Grid<float> velocityX, Grid<float> velocityY, Grid<float> tempDivergance, Grid<float> tempPressure) {
+    tempDivergance.Fill(0);
+    tempPressure.Fill(0);
+    
     float a = 1.0f; // a = 1.0f / N
     for (int r = 1; r <= h; ++r) {
         for (int c = 1; c <= w; ++c) {
+            if (wall[c, r]) continue;
             tempDivergance[c, r] = -0.5f * a * (
                 +velocityX[c + 1, r] - velocityX[c - 1, r]
                 +velocityY[c, r + 1] - velocityY[c, r - 1]
             );
-            tempPressure[c, r] = 0.0f;
         }
     }
-    SetBoundary(BoundaryMode.Normal, w, h, tempDivergance);
-    SetBoundary(BoundaryMode.Normal, w, h, tempPressure);
+    SetBoundary(wall, BoundaryMode.Normal, w, h, tempDivergance);
+
 
     for (int iter = 0; iter < iteration_count; ++iter) {
         for (int r = 1; r <= h; ++r) {
             for (int c = 1; c <= w; ++c) {
+                if (wall[c, r]) continue;
                 tempPressure[c, r] = 0.25f * (
                     tempDivergance[c, r] + 
                     tempPressure[c + 1, r] + tempPressure[c - 1, r] + 
@@ -98,10 +176,11 @@ static void Project(int w, int h, Grid<float> velocityX, Grid<float> velocityY, 
             velocityY[c, r] -= 0.5f / a * (tempPressure[c, r + 1] - tempPressure[c, r - 1]);
         }
     }
-    SetBoundary(BoundaryMode.VelocityX, w, h, velocityX);
-    SetBoundary(BoundaryMode.VelocityX, w, h, velocityY);
+    SetBoundary(wall, BoundaryMode.VelocityX, w, h, velocityX);
+    SetBoundary(wall, BoundaryMode.VelocityX, w, h, velocityY);
 }
 
+Grid<bool> wall       = new(width_with_border, height_with_border, false);
 Grid<float> density   = new(width_with_border, height_with_border, 0);
 Grid<float> velocityX = new(width_with_border, height_with_border, 0);
 Grid<float> velocityY = new(width_with_border, height_with_border, 0);
@@ -110,22 +189,22 @@ Grid<float> tempY     = new(width_with_border, height_with_border, 0);
 
 
 void Simulate(float dt) {
-    Diffuse(BoundaryMode.Normal, width, height, dt, density_diffusion_rate, density, tempX);
+    Diffuse(wall, BoundaryMode.Normal, width, height, dt, density_diffusion_rate, density, tempX);
     (density, tempX) = (tempX, density);
-    Advect(BoundaryMode.Normal, width, height, dt, velocityX, velocityY, density, tempX);
+    Advect(wall, BoundaryMode.Normal, width, height, dt, velocityX, velocityY, density, tempX);
     (density, tempX) = (tempX, density);
 
-    Diffuse(BoundaryMode.VelocityX, width, height, dt, velocity_viscosity_rate, velocityX, tempX);
-    Diffuse(BoundaryMode.VelocityY, width, height, dt, velocity_viscosity_rate, velocityY, tempY);
+    Diffuse(wall, BoundaryMode.VelocityX, width, height, dt, velocity_viscosity_rate, velocityX, tempX);
+    Diffuse(wall, BoundaryMode.VelocityY, width, height, dt, velocity_viscosity_rate, velocityY, tempY);
     (velocityX, tempX) = (tempX, velocityX);
     (velocityY, tempY) = (tempY, velocityY);
-    Project(width, height, velocityX, velocityY, tempX, tempY);
+    Project(wall, width, height, velocityX, velocityY, tempX, tempY);
 
-    Advect(BoundaryMode.VelocityX, width, height, dt, velocityX, velocityY, velocityX, tempX);
-    Advect(BoundaryMode.VelocityY, width, height, dt, velocityX, velocityY, velocityY, tempY);
+    Advect(wall, BoundaryMode.VelocityX, width, height, dt, velocityX, velocityY, velocityX, tempX);
+    Advect(wall, BoundaryMode.VelocityY, width, height, dt, velocityX, velocityY, velocityY, tempY);
     (velocityX, tempX) = (tempX, velocityX);
     (velocityY, tempY) = (tempY, velocityY);
-    Project(width, height, velocityX, velocityY, tempX, tempY);
+    Project(wall, width, height, velocityX, velocityY, tempX, tempY);
 }
 
 Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
@@ -147,8 +226,12 @@ F1 - show help
 F2 - show grid
 F3 - show velocity field
 R - reset
+LMB - add density
+RMB - add wall
+W,S,A,D - add velocity
 Q, E - change added density
 LCTRL, LSHIFT - change added velocity
+
 """.ReplaceLineEndings("\n");
 
 while (!Raylib.WindowShouldClose()) {
@@ -199,11 +282,24 @@ while (!Raylib.WindowShouldClose()) {
         int c = (int)cell.X;
         int r = (int)cell.Y;
 
+        if (Raylib.IsMouseButtonDown(MouseButton.Right)) {
+            for (int x = 0; x < 3; ++x) {
+                for (int y = 0; y < 3; ++y) {
+                    int c0 = c + x;
+                    int r0 = r + y;
+
+                    if (0 <= c0 && c0 < width_with_border && 0 <= r0 && r0 < height_with_border) {
+                        wall[c0, r0] = true; 
+                    } 
+                }
+            }
+        }
+
         if (1 <= c && c <= width && 1 <= r && r <= height) {  
             if (Raylib.IsMouseButtonDown(MouseButton.Left)) {
                 density[c, r] += Raylib.GetFrameTime() * densityAddedPerSecond; 
             }
-            
+               
             if (Raylib.IsKeyDown(KeyboardKey.W)) {
                 velocityY[c, r] -= Raylib.GetFrameTime() * velocityAddedPerSecond;
             }
@@ -243,11 +339,14 @@ while (!Raylib.WindowShouldClose()) {
             Color colour = new(intensity, 0, 0);
 
             Vector2 pos = cellSize * new Vector2(c, r);
-
             Rectangle cell = new(pos, cellSize);
             Raylib.DrawRectangleRec(cell, colour);
             if (showGrid) {
                 Raylib.DrawRectangleLinesEx(cell, 1.0f, new(50, 50, 50));
+            }
+
+            if (wall[c, r]) {
+                Raylib.DrawRectangleLinesEx(cell, 1.0f, Color.Green);
             }
         }
     }
@@ -260,7 +359,7 @@ while (!Raylib.WindowShouldClose()) {
                 float len = vel.Length();
                 if (len > 1e-10) {
                     Vector2 dir = vel * (cellSize.X * 0.5f / len);
-                    Raylib.DrawLineV(center, center + vel * cellSize * 0.1f, Color.Magenta);
+                    Raylib.DrawLineV(center, center + vel * cellSize * fixedUpdateDelta, Color.Magenta);
                     // Raylib.DrawLineV(center, center + dir, Color.Blue);
                 } 
             }
@@ -283,7 +382,10 @@ readonly struct Grid<T> {
     public Grid(int width, int height, T initialValue) {
         this.width = width;
         array = new T[width * height];
-        Array.Fill(array, initialValue);
+        Fill(initialValue);
+    }
+    public void Fill(T value) {
+        Array.Fill(array, value);
     }
     public readonly ref T this[int c, int r] => ref array[width * r + c];
 };
